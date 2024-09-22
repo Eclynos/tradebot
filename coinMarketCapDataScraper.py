ae30dc90c8bc42af5b40b93150974b6cce62412e
Nicolas ANGLADE <nicolas.anglade63@gmail.com>
2024-09-22 09:37:16 +0200

    Improved performance using multiThreading

--- coinMarketCapDataScraper.py
+++ coinMarketCapDataScraper.py
@@ -1,18 +1,24 @@
 from tools import *
-import time
+import threading
+
+def testWorth(key, t):
+    print(key, " : ", t.isWorthBuying(key, 50000))
 
 if __name__ == "__main__":
-    startTime=time.time()
-
     codeToIDDico = {}
     with open('./codeToID.json','r') as json_File :
         codeToIDDico=json.load(json_File)
 
-    t = Tools(codeToIDDico)
-    for k in codeToIDDico.keys():
-        print(k, " : ", t.isWorthBuying(k, 50000))
+    numberOfCryptos = len(list(codeToIDDico.keys()))
 
-    print("finished scanning :", len(list(codeToIDDico.keys())), "cryptos in", time.time() - startTime, "s.")
+    t = Tools(codeToIDDico)
+    threadList = []
+
+    for k in codeToIDDico.keys():
+        threadList.append(threading.Thread(target=testWorth, args=(k,t)))
+
+    for i in range(numberOfCryptos):
+        threadList[i].start()
 
     # print(t.getRealMins(t.getMathLocalyMins(t.getCoinData("ETH", "7D"), "7D"), 50000))
     # print(t.isInDrop("KAS", "7D", 50000))

6045c8992eaf545f75722da0902c4168839fb19f
Eclynos <Eclynos@outlook.com>
2024-09-25 10:20:06 +0200

    Modifications des fonctions de trading Wallet et ajout de fonctions de regard sur les transactions

--- tools.py
+++ tools.py
@@ -2,7 +2,7 @@ import requests
 import json
 import time
 import numpy
-import ccxt
+import ccxt.pro as ccxt
 
 class Tools:
     def __init__(self, IDDico) -> None:
@@ -167,45 +167,79 @@ class Wallet:
         self.access_key = access_key
         self.secret_key = secret_key
         self.passphrase = passphrase
+        
         self.exchange = ccxt.bitget({ # etablie la connexion au compte
         'apiKey': access_key,
         'secret': secret_key,
         'password': passphrase,
         })
+        
         self.exchange.set_sandbox_mode(True) # Le mode sandbox permet de tester des stratégies de trading ou d'effectuer des opérations fictives dans un environnement de simulation sans engager de fonds réels. À utiliser pour tester l'api
         balance = self.exchange.fetch_balance() # effectuer les opérations dans l'environnement test (sandbox)
         self.exchange.verbose = True # pour le debug
         
+        
     async def place_order(self, coinCode, BuyorSell, amount, price):
+        """Place un ordre d'acheter ou vendre lorsque la crypto atteint un certain prix"""
         await self.exchange.load_markets() # met en cache toutes les informations sur les paires de trading disponibles avant d'effectuer des opérations de trading
+        
         orders = await self.exchange.create_orders({
-                'symbol': ''+'/USDT:USDT', # trouver quelque chose pour remplir les symboles, le but est d'obtenir par exemple : 'ETH/USDT:USDT'
+                'symbol': coinCode +'/USDT:USDT', # trouver quelque chose pour remplir les symboles, le but est d'obtenir par exemple : 'ETH/USDT:USDT'
                 'type': 'limit',
                 'side': BuyorSell,
                 'amount': amount,
                 'price': price,
             })
         print(orders)
+        
     
     async def buy(self, coinCode, amount):
+        """Achète directement une crypto"""
         await self.exchange.load_markets() # met en cache toutes les informations sur les paires de trading disponibles avant d'effectuer des opérations de trading
+        
         orders = await self.exchange.create_orders({
-                'symbol': ''+'/USDT:USDT', # trouver quelque chose pour remplir les symboles, le but est d'obtenir par exemple : 'ETH/USDT:USDT'
+                'symbol': coinCode +'/USDT:USDT', # trouver quelque chose pour remplir les symboles, le but est d'obtenir par exemple : 'ETH/USDT:USDT'
                 'type': 'market',
                 'side': 'buy',
                 'amount': amount,
             })
         print(orders)
+        
     
     async def sell(self, coinCode, amount): # on pourra faire en sorte de sell un pourcentage et pas un amount
+        """Vend directement une crypto"""
         await self.exchange.load_markets() # met en cache toutes les informations sur les paires de trading disponibles avant d'effectuer des opérations de trading
+        
         orders = await self.exchange.create_orders({
-            'symbol': ''+'/USDT:USDT', # trouver quelque chose pour remplir les symboles, le but est d'obtenir par exemple : 'ETH/USDT:USDT'
+            'symbol': coinCode +'/USDT:USDT', # trouver quelque chose pour remplir les symboles, le but est d'obtenir par exemple : 'ETH/USDT:USDT'
             'type': 'market',
             'side': 'sell',
             'amount': amount,
         })
         print(orders)
+      
+      
+    async def watchPositions(self, coinCode):
+        """Regarde les positions actuelles pour un symbole"""
+        trades = await self.exchange.watch_positions(coinCode + '/USDT:USDT') # si l'argument est None, donne toutes les positions sur chaque symbole
+        print(trades)
+        return trades  
+    
         
-    def disconnect(self): # à faire à la fin de l'utilisation, à la fin du code
-        self.exchange.close()
+    async def transactionHistory(self, coinCode):
+        """Donne l'historique des trades sur un symbole (crypto)"""
+        trades = await self.exchange.fetch_my_trades(coinCode + '/USDT')
+        
+        for trade in trades:
+            print(f"Trade ID: {trade['id']}, Type: {trade['side']}, Prix: {trade['price']}, Quantité: {trade['amount']}, Date: {self.exchange.iso8601(trade['timestamp'])}")
+    
+       
+    async def orderBook(self, coinCode):
+        """Donne l'order book des trades sur un symbole""" # Voir diff avec history
+        orderbook = await self.exchange.watch_order_book_for_symbols(coinCode + '/USDT')
+        print(orderbook['symbol'], orderbook['asks'][0], orderbook['bids'][0])
+
+        
+    async def disconnect(self): # à faire à la fin de l'utilisation du compte, à la fin du code
+        """Déconnecte le compte lié"""
+        await self.exchange.close()

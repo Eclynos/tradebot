f66ade6ad7cf8c21a8fa82ba83bcc05f2fc4176a
Nicolas ANGLADE <nicolas.anglade63@gmail.com>
2024-10-28 11:36:23 +0100

    added faster batch backtest

--- backtestMain.py
+++ backtestMain.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     s = Strategy()
     da = DataAnalysis()
     t = Tools()
-    coinCode = "XRP"
+    coinCode = "BTC"
 
     allData = t.readFile(coinCode) # Il faut avoir téléchargé le fichier avec Backtest-Tools-V2 au préalable et le placer dans ./data/
 
@@ -17,19 +17,10 @@ if __name__ == "__main__":
     for i in range(len(allData)):
         usableData.append({"date": int(allData[i]["date"])//1000, "price": float(allData[i]["close"]), "index" :i})
 
-
-
-    tradeList = []
-    startIndex = -1
-    endIndex = -1
+    startTime = time.time()
     currentDate = int(time.time())
-    for i in range(len(usableData)):
-        if startIndex != -1 and endIndex != -1:
-            break
-        if currentDate - usableData[i]["date"] < t.time_frame_to_s("9M") and endIndex == -1:
-            endIndex = i 
-        if currentDate - usableData[i]["date"] < t.time_frame_to_s("1y") and startIndex == -1:
-            startIndex = i
+    startIndex = t.binarySearch(usableData, currentDate-t.time_frame_to_s("6M"), "date")
+    endIndex = t.binarySearch(usableData, currentDate-t.time_frame_to_s("3M"), "date")
 
     if endIndex == -1:
         endIndex = len(usableData) -1
@@ -38,48 +29,45 @@ if __name__ == "__main__":
         print(f"Pas assez de données sur la coin {coinCode} !")
         exit(0)
 
-    print("début des fonctions")
-    startTime = time.time()
-    ma = da.exponentialMovingAverage(usableData[startIndex:endIndex], 100)
-    print("ma in :", time.time()-startTime)
-    startTime = time.time()
-    sd = da.expoStandardDeviation(usableData[startIndex:endIndex], ma, 100)
-    print("sd in :", time.time()-startTime)
-    startTime = time.time()
-    avgsd = da.simpleWeightedAverage(sd, 2000)
-    print("avgsd in :", time.time()-startTime)
-    exit(0)
-
-    for i in range(startIndex, endIndex-1):
-        if (s.buyingEvaluation(usableData[i-2000:i+1], usableData[i]["date"]) 
-            and (len(tradeList) == 0 or usableData[i]["date"] > tradeList[-1]["date"] + t.time_frame_to_s("0m"))):
+    tradeList = []
+    tradeTimeList = s.batchBuyingEvaluation(usableData[startIndex-100:endIndex])
+    ttlIndex = 0
+    for i in range(startIndex, endIndex):
+        if usableData[i]["date"] == tradeTimeList[ttlIndex]:
             tradeList.append(usableData[i])
+            ttlIndex += 1
+        if ttlIndex == len(tradeTimeList):
+            break
+    
+    # tradeList = []
+    # for i in range(startIndex, endIndex-1):
+    #     if (s.buyingEvaluation(usableData[i-2000:i+1], usableData[i]["date"]) 
+    #         and (len(tradeList) == 0 or usableData[i]["date"] > tradeList[-1]["date"] + t.time_frame_to_s("0m"))):
+    #         tradeList.append(usableData[i])
         
-        if i%10000 == 0:
-            print("buying progress :", 100*(i-startIndex)/(endIndex-startIndex), "%")
+    #     if i%10000 == 0:
+    #         print("buying progress :", 100*(i-startIndex)/(endIndex-startIndex), "%")
 
-    print(len(tradeList))
+    print(time.time()-startTime, "s")
 
     sell = s.sellingEvaluation(usableData[startIndex: endIndex], tradeList)
+    print(sell)
 
-    print("\n")
+    print()
     print("profit w fees deducted :", 100*(sell[1]-1), "%\t % of positive trades", 100*sell[2], "\t number of trades :", sell[3])
     totalTradeTime = usableData[endIndex-1]["date"] - usableData[startIndex]["date"]
     print(totalTradeTime, "sec. =", totalTradeTime / 60, "min. =", totalTradeTime / 3600, "h. =", totalTradeTime / 86400, "jours =", totalTradeTime / 604800, "sem. =", totalTradeTime / (365.25 * 86400), "ans")
     print("starting price :", usableData[startIndex]["price"], "\t finish price :", usableData[endIndex-1]["price"], "\t diff :", 100*(usableData[endIndex-1]["price"]-usableData[startIndex]["price"])/(usableData[startIndex]["price"]), "%")
+    
+    mp = da.minPrice(usableData[startIndex:endIndex])
+    bb15 = da.bollinger(s.ma, s.sd, 1.5)
+    showsd = [{"date":s.sd[i]["date"], "price": mp + s.sd[i]["price"]} for i in range(len(s.sd))]
+    avgsd = da.simpleWeightedAverage(s.sd, 1000)
+    showavgsd = [{"date":avgsd[i]["date"], "price" : mp + avgsd[i]["price"]} for i in range(len(avgsd))]
 
-    # ma = da.exponentialMovingAverage(usableData[startIndex:endIndex], 100)
-    # sd = da.expoStandardDeviation(usableData[startIndex:endIndex], ma, 100)
-
-    # mp = da.minPrice(usableData[startIndex:endIndex])
-    # bb15 = da.bollinger(ma, sd, 1.5)
-    # showsd = [{"date":sd [i]["date"], "price": mp + sd[i]["price"]} for i in range(len(sd))]
-    # avgsd = da.simpleWeightedAverage(sd, 1000)
-    # showavgsd = [{"date":avgsd[i]["date"], "price" : mp + avgsd[i]["price"]} for i in range(len(avgsd))]
-
-    # da.visualisation(coinCode, usableData[startIndex: endIndex], "curve",
-    #                            ma, "curve",
-    #                            bb15, "curve",
-    #                            showsd, "curve",
-    #                            showavgsd, "curve",
-    #                            sell[0], "buy-sell")
+    da.visualisation(coinCode, usableData[startIndex: endIndex], "curve",
+                               s.ma, "curve",
+                               bb15, "curve",
+                               showsd, "curve",
+                               showavgsd, "curve",
+                               sell[0], "buy-sell")
--- strategyStandardDevPump.py
+++ strategyStandardDevPump.py
@@ -6,9 +6,28 @@ class Strategy:
         self.dA = DataAnalysis()
         self.t = Tools()
         self.sd=[]
+        self.ma = []
         self.sdWeightedAvg = []
         self.movingAverageSize = 100
         self.weightedAvgSize = 2000
+
+    def batchBuyingEvaluation(self, data):
+        self.ma = self.dA.exponentialMovingAverage(data, self.movingAverageSize, 0.95)
+        self.sd = self.dA.fastExponentialStandardDeviation(data, self.movingAverageSize, 0.95, 0.95)
+        self.sdWeightedAvg = self.dA.simpleWeightedAverage(self.sd, self.weightedAvgSize)
+        bb = self.dA.bollinger(self.ma, self.sd, 1.5)
+        
+        buyTimes = []
+        for i in range(self.weightedAvgSize+self.movingAverageSize, len(data)-2):
+            if (self.sd[i-self.movingAverageSize]["price"] > self.sdWeightedAvg[i-self.weightedAvgSize-self.movingAverageSize+1]["price"] 
+            and self.sd[i-self.movingAverageSize-1]["price"] < self.sdWeightedAvg[i-self.weightedAvgSize-self.movingAverageSize]["price"] 
+            and self.dA.trend(data[i-self.movingAverageSize+1:i+1], 1/2) == 1
+            and data[i]["price"] > bb[i-self.movingAverageSize]["price"]
+            ):
+                buyTimes.append(data[i]["date"])
+
+        return buyTimes
+            
 
     def buyingEvaluation(self, data, time):
         ma = self.dA.exponentialMovingAverage(data[-self.movingAverageSize-1:], self.movingAverageSize, 0.95)
@@ -41,18 +60,18 @@ class Strategy:
             hasPassedUnder0 = False
             for j in range(len(data)):
                 if (data[j]["date"] > tradeList[0]["date"] and
-                    data[j]["price"] < bb[j-self.movingAverageSize]["price"]):
+                    data[j]["price"] < bb[j-self.movingAverageSize+1]["price"]):
                     hasPassedUnder0 = True
                 if (j-self.weightedAvgSize > 0 and
                     data[j]["date"] > tradeList[0]["date"] and
-                    self.sd[j]["price"] < self.sdWeightedAvg[j-self.weightedAvgSize]["price"]
-                    and self.sd[j-1]["price"] > self.sdWeightedAvg[j-self.weightedAvgSize-1]["price"]
+                    self.sd[j-self.movingAverageSize+1]["price"] < self.sdWeightedAvg[j-self.movingAverageSize-self.weightedAvgSize+2]["price"]
+                    and self.sd[j-self.movingAverageSize]["price"] > self.sdWeightedAvg[j-self.movingAverageSize-self.weightedAvgSize+1]["price"]
                     
                     or 
 
                     hasPassedUnder0 and
                     data[j]["date"] > tradeList[0]["date"] and
-                    data[j]["price"] > bb2[j-self.movingAverageSize]["price"]
+                    data[j]["price"] > bb2[j-self.movingAverageSize+1]["price"]
                     ):
 
                     sellIndex = j

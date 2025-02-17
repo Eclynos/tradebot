b8a123e36f4ae9f1662a6269ba7f34b25db3e9f6
Nathaël CARLIER <Nathael.CARLIER@etu.uca.fr>
2025-02-17 17:05:15 +0100

    float64 : x3 faster

--- nodeApproxAutoParam.py
+++ nodeApproxAutoParam.py
@@ -12,6 +12,7 @@ NB_RECURSIONS = 2
 REDUCTION_PAR_ETAPE = 0.5
 START_TIME = "8M"
 END_TIME = "6M"
+FLOAT_TYPE = tf.float64
 
 coinCodes = [
     "SOL"
@@ -28,26 +29,13 @@ data = [[{"date": int(data[i][j]["date"]) // 1000, "price": float(data[i][j]["cl
 STRATEGY_NAME = "dip"
 PERCENTAGE_TRADED = 0.25
 
-"""
 INSTANCES_BOUNDS = {
-    "power1" : [0.8, 1],
-    "power2" : [0.8, 1],
-    "buyingBollinger": [0, 3],
-    "sellingBollinger1": [-1, 1],
-    "sellingBollinger2": [0, 3]
+    "power1": [tf.constant(0.8, dtype=FLOAT_TYPE), tf.constant(1.0, dtype=FLOAT_TYPE)],
+    "power2": [tf.constant(0.8, dtype=FLOAT_TYPE), tf.constant(1.0, dtype=FLOAT_TYPE)],
+    "buyingBollinger": [tf.constant(0.0, dtype=FLOAT_TYPE), tf.constant(3.0, dtype=FLOAT_TYPE)],
+    "sellingBollinger1": [tf.constant(-1.0, dtype=FLOAT_TYPE), tf.constant(1.0, dtype=FLOAT_TYPE)],
+    "sellingBollinger2": [tf.constant(0.0, dtype=FLOAT_TYPE), tf.constant(3.0, dtype=FLOAT_TYPE)]
 }
-"""
-
-
-INSTANCES_BOUNDS = {
-    "power1": [tf.constant(0.8, dtype=tf.float32), tf.constant(1.0, dtype=tf.float32)],
-    "power2": [tf.constant(0.8, dtype=tf.float32), tf.constant(1.0, dtype=tf.float32)],
-    "buyingBollinger": [tf.constant(0.0, dtype=tf.float32), tf.constant(3.0, dtype=tf.float32)],
-    "sellingBollinger1": [tf.constant(-1.0, dtype=tf.float32), tf.constant(1.0, dtype=tf.float32)],
-    "sellingBollinger2": [tf.constant(0.0, dtype=tf.float32), tf.constant(3.0, dtype=tf.float32)]
-}
-
-
 
 s = {cc: None for cc in coinCodes}
 
@@ -59,23 +47,17 @@ def bestPoint(cc, nb_points_testes, bornes, s, data):
 
     s[cc].candles = data[cc]
 
-    power1 = tf.Variable(bornes["power1"][0], dtype=tf.float32)
-    power2 = tf.Variable(bornes["power2"][0], dtype=tf.float32)
-    buyingBollinger = tf.Variable(bornes["buyingBollinger"][0], dtype=tf.float32)
-    sellingBollinger1 = tf.Variable(bornes["sellingBollinger1"][0], dtype=tf.float32)
-    sellingBollinger2 = tf.Variable(bornes["sellingBollinger2"][0], dtype=tf.float32)
+    power1 = tf.Variable(bornes["power1"][0], dtype=FLOAT_TYPE)
+    power2 = tf.Variable(bornes["power2"][0], dtype=FLOAT_TYPE)
+    buyingBollinger = tf.Variable(bornes["buyingBollinger"][0], dtype=FLOAT_TYPE)
+    sellingBollinger1 = tf.Variable(bornes["sellingBollinger1"][0], dtype=FLOAT_TYPE)
+    sellingBollinger2 = tf.Variable(bornes["sellingBollinger2"][0], dtype=FLOAT_TYPE)
 
-    power1_values = tf.linspace(bornes["power1"][0], bornes["power1"][1], nb_points_testes)
-    power2_values = tf.linspace(bornes["power2"][0], bornes["power2"][1], nb_points_testes)
-    buyingBollinger_values = tf.linspace(bornes["buyingBollinger"][0], bornes["buyingBollinger"][1], nb_points_testes)
-    sellingBollinger1_values = tf.linspace(bornes["sellingBollinger1"][0], bornes["sellingBollinger1"][1], nb_points_testes)
-    sellingBollinger2_values = tf.linspace(bornes["sellingBollinger2"][0], bornes["sellingBollinger2"][1], nb_points_testes)
-
-    for p1 in power1_values:
-        for p2 in power2_values:
-            for bb in buyingBollinger_values:
-                for sb1 in sellingBollinger1_values:
-                    for sb2 in sellingBollinger2_values:
+    for p1 in tf.linspace(bornes["power1"][0], bornes["power1"][1], nb_points_testes):
+        for p2 in tf.linspace(bornes["power2"][0], bornes["power2"][1], nb_points_testes):
+            for bb in tf.linspace(bornes["buyingBollinger"][0], bornes["buyingBollinger"][1], nb_points_testes):
+                for sb1 in tf.linspace(bornes["sellingBollinger1"][0], bornes["sellingBollinger1"][1], nb_points_testes):
+                    for sb2 in tf.linspace(bornes["sellingBollinger2"][0], bornes["sellingBollinger2"][1], nb_points_testes):
                         power1.assign(p1)
                         power2.assign(p2)
                         buyingBollinger.assign(bb)
@@ -98,6 +80,7 @@ def bestPoint(cc, nb_points_testes, bornes, s, data):
 
     return best_params, best_yield
 
+
 def main():
     st = time.time()
     bounds_copy = INSTANCES_BOUNDS.copy()
@@ -114,6 +97,7 @@ def main():
             print(recursion)
 
         print(f"The best params for {coinCodes[cc]} are:\n{str(meilleurParam)}\nyield: {meilleurYield}\nCalculation time: {time.time() - st}")
+
 
 if __name__ == "__main__":
     gpus = tf.config.experimental.list_physical_devices('GPU')
